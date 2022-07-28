11571ff78d1a2f8bcac5b0c597c0f21e8241ea4d
GuasaPlay <gameplaysoscar@gmail.com>
2022-07-28 15:23:06 -0500

    Added home route

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const conectarDB = require('./config/db');
-const cors = require("cors");
-
+const cors = require('cors');
 
 // Creamos el servidor
 const app = express();
 
 // Conectamos a la BD
 conectarDB();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 const newLocal = '/api/Customer';
 app.use(newLocal, require('./routes/customer'));
 
+app.get('/', (req, res) => {
+	res.json({
+		msg: 'TODO OK',
+	});
+});
+
 app.listen(4000, () => {
-    console.log('El servidor esta corriendo perfectamente')
-})
-
-
-// app.set('port',process.env.DB_MONGO )
-// app.use(express.urlencoded({extends: true}));
-
-// app.use(fileUpload());
-
+	console.log('El servidor esta corriendo perfectamente');
+});
